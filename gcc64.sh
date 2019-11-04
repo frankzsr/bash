e390a43d4d35977008f6033e0c4ddeeeb75edebc
frankzsr <zhaosongru_frank@163.com>
2019-11-04 10:21:33 +0800

    modified gcc64.sh

--- gcc64.sh
+++ gcc64.sh
@@ -7,6 +7,10 @@
 #/usr/bin/ld:
 #ld.bin -m elf_i386 $@
 
+#另外说明下在ubuntu上如果要用 -m32 参数就要安装如下的库：
+# sudo apt-get install build-essential module-assistant -y
+# sudo apt-get install gcc-multilib g++-multilib -y
+
 sudo mv -i /usr/bin/gcc /usr/bin/gcc.sh
 sudo mv -i /usr/bin/ld  /usr/bin/ld.sh
 
